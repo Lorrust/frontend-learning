ee50067ee33047a005a01cecb213bc08a6d1fcc9
Lucas <lucasrs202@gmail.com>
2024-09-12 16:22:11 -0300

    Complete to-do list

--- aula-4/todo-list/script.js
+++ aula-4/todo-list/script.js
@@ -1,29 +1,41 @@
 function addItem(event) {
-
     event.preventDefault();
-    console.log(event)
+    console.log(event);
 
     const itemList = document.getElementById('list-container');
     const itemInput = document.getElementById('item-input');
 
-    if (itemInput.value.trim().length > 0 ) {
+    if (itemInput.value.trim().length > 0) {
         const newItem = document.createElement('div');
         newItem.classList.add('item');
         newItem.innerHTML =
             `<p>${itemInput.value}</p>
             <button onclick="completeTask(event)">Done</button>
-            <button onclick="deleteTask(event)">Delete</button>`
-        
+            <button onclick="deleteTask(event)">Delete</button>`;
+
         itemList.appendChild(newItem);
+        updateTaskCount(); // Call the function to update the task count
     }
     itemInput.value = "";
     itemInput.focus();
+}
+
+function updateTaskCount() {
+    const totalCount = Array.from(document.getElementById('list-container').children).length;
+    console.log(totalCount)
+    const doneText = document.getElementById('done-count');
+    const todoText = document.getElementById('todo-count');
+    const completeAmount = document.getElementsByClassName('complete').length;
+
+    doneText.textContent = `Complete: ${completeAmount}`;
+    todoText.textContent = `Remaining: ${totalCount - completeAmount}`;
 }
 
 function deleteTask(event) {
     console.log(event);
     const task = event.target.parentNode;
     task.parentNode.removeChild(task);
+    updateTaskCount();
 }
 
 function clearDone(event) {
@@ -37,11 +49,12 @@ function clearDone(event) {
         console.log(item.parentNode)
         itemList.removeChild(item.parentNode);
     }
-
+    updateTaskCount()
 }
 
 function completeTask(event) {
     const task = event.target.parentNode;
     const taskName = task.childNodes[0];
     taskName.classList.toggle('complete');
+    updateTaskCount()
 }
